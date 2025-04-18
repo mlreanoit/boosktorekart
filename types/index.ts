7b12d7b3741401afbdfc4b0e06d99248dae3a6d5
LEGION <LEGION@LEGION-MLR>
2025-04-18 15:47:24 +0800

    18-place order

--- types/index.ts
+++ types/index.ts
@@ -3,6 +3,7 @@ import {
   OrderItemSchema,
   UserInputSchema,
   UserSignUpSchema,
+  OrderInputSchema,
   ShippingAddressSchema,
   UserSignInSchema,
   ProductInputSchema,
@@ -27,6 +28,7 @@ export type Data = {
   }[]
 }
 
+export type IOrderInput = z.infer<typeof OrderInputSchema>
 export type OrderItem = z.infer<typeof OrderItemSchema>
 export type Cart = z.infer<typeof CartSchema>
 export type ShippingAddress = z.infer<typeof ShippingAddressSchema>
