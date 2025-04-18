5a1a08f377c9828d65da86a482223f415790cac5
LEGION <LEGION@LEGION-MLR>
2025-04-18 11:44:14 +0800

    11- implement add to cart page

--- lib/constants.ts
+++ lib/constants.ts
@@ -6,3 +6,6 @@ export const APP_DESCRIPTION =
   'A bookstore app built with Next.js and MongoDB'
 
 export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9)
+export const FREE_SHIPPING_MIN_PRICE = Number(
+  process.env.FREE_SHIPPING_MIN_PRICE || 35
+)
